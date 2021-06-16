3adf0afb58cad5a3769a23e364f6486e8a98fff4
aitalina <aita.m@bk.ru>
2021-06-16 13:27:26 +0900

    create

---
+++ src/main/java/com/example/ynga/YngaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ynga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YngaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YngaApplication.class, args);
+    }
+
+}
